80eacadaa72564d0dab1129922f6e899345bed5f
Abi1022 <abishek.khadka@varosatech.com>
2026-02-09 00:04:54 +0545

    debugging ci/cd is picking latest change or not

--- src/main/java/com/institute/listing/ListingApplication.java
+++ src/main/java/com/institute/listing/ListingApplication.java
@@ -3,10 +3,12 @@ package com.institute.listing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ListingApplication {
 
 	public static void main(String[] args) {
+		System.out.println("CICD is running");
 		SpringApplication.run(ListingApplication.class, args);
 	}
 
